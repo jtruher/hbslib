cd0a4c42dd534717a7b77a417874167806836c37
Jim Truher <jtruher@gmail.com>
2020-06-25 12:47:43 -0700

    adding a unique method

--- Sources/hbslib/hbslib.swift
+++ Sources/hbslib/hbslib.swift
@@ -33,3 +33,11 @@ public extension UIImage {
         return image!
     }
 }
+
+public extension Sequence where Iterator.Element: Hashable {
+    func unique() -> [Iterator.Element] {
+        var seen: Set<Iterator.Element> = []
+        return filter { seen.insert($0).inserted }
+    }
+}
+
