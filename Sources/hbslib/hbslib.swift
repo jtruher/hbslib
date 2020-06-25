839683b5d395e881612cd65e227325a43451c0e7
Jim Truher <jtruher@gmail.com>
2020-06-24 17:04:01 -0700

    Adding two testy boys

--- Sources/hbslib/hbslib.swift
+++ Sources/hbslib/hbslib.swift
@@ -1,3 +1,34 @@
+import UIKit
+
 struct hbslib {
     var text = "Hello, World!"
+}
+
+extension UIView {
+    func applyAutolayoutHugging() {
+        if let sv = self.superview {
+            self.translatesAutoresizingMaskIntoConstraints = false
+
+            NSLayoutConstraint.activate([
+                self.topAnchor.constraint(equalTo: sv.topAnchor),
+                self.bottomAnchor.constraint(equalTo: sv.bottomAnchor),
+                self.leftAnchor.constraint(equalTo: sv.leftAnchor),
+                self.rightAnchor.constraint(equalTo: sv.rightAnchor),
+            ])
+        }
+    }
+}
+
+extension UIImage {
+    func toBase64() -> String? {
+        guard let imageData = self.pngData() else { return nil }
+        return imageData.base64EncodedString(options: Data.Base64EncodingOptions.lineLength64Characters)
+    }
+    
+    static func convertBase64StringToImage(imageBase64String : String) -> UIImage {
+        let imageData = Data.init(base64Encoded: imageBase64String, options: .init(rawValue: 0))
+        let image = UIImage(data: imageData!)
+        
+        return image!
+    }
 }
