9ec72502dcb09447625ec23448ab8c120d51b77e
Jim Truher <jtruher@gmail.com>
2020-11-21 16:52:17 -0800

    uibutton closure

--- Sources/hbslib/hbslib.swift
+++ Sources/hbslib/hbslib.swift
@@ -46,6 +46,59 @@ public extension UIFont {
         return UIFont.preferredFont(forTextStyle: .body)
     }
 
+    static func title2() -> UIFont {
+        return UIFont.preferredFont(forTextStyle: .title2)
+    }
+
+    static func headline() -> UIFont {
+        return UIFont.preferredFont(forTextStyle: .headline)
+    }
+
+}
+
+typealias UIButtonTargetClosure = (UIButton) -> Void
+
+class ClosureWrapper: NSObject {
+    let closure: UIButtonTargetClosure
+    init(_ closure: @escaping UIButtonTargetClosure) {
+        self.closure = closure
+    }
+}
+
+extension UIButton {
+    private struct AssociatedKeys {
+        static var targetClosure = "targetClosure"
+    }
+
+    private var targetClosure: UIButtonTargetClosure? {
+        get {
+            guard let closureWrapper = objc_getAssociatedObject(self, &AssociatedKeys.targetClosure)
+                    as? ClosureWrapper else { return nil }
+            return closureWrapper.closure
+        }
+        set(newValue) {
+            guard let newValue = newValue else { return }
+            objc_setAssociatedObject(self,
+                                     &AssociatedKeys.targetClosure,
+                                     ClosureWrapper(newValue),
+                                     objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+        }
+    }
+
+    func addTargetClosure(closure: @escaping UIButtonTargetClosure) {
+        targetClosure = closure
+        addTarget(self, action: #selector(UIButton.closureAction), for: .touchUpInside)
+    }
+
+    func addTargetClosure(eventType: UIControl.Event, closure: @escaping UIButtonTargetClosure) {
+        targetClosure = closure
+        addTarget(self, action: #selector(UIButton.closureAction), for: eventType)
+    }
+
+    @objc func closureAction() {
+        guard let targetClosure = targetClosure else { return }
+        targetClosure(self)
+    }
 }
 
 public extension UIColor {
