aacc08b95256d1dec30091d6ddadbd5aabbb638b
Jim Truher <jtruher@gmail.com>
2021-08-08 12:48:52 -0700

    Minor fixes, hopefully to get xcode 13 to quiet down

--- Sources/hbslib/hbslib.swift
+++ Sources/hbslib/hbslib.swift
@@ -132,7 +132,7 @@ import GameplayKit
 /// let randomChar = chars.randomElement(using: &generator)
 /// let randomInt = Int.random(in: 0 ..< 1000, using: &generator)
 /// ```
-class SeededGenerator: RandomNumberGenerator {
+public class SeededGenerator: RandomNumberGenerator {
     let seed: UInt64
     private let generator: GKMersenneTwisterRandomSource
     convenience init() {
@@ -142,11 +142,14 @@ class SeededGenerator: RandomNumberGenerator {
         self.seed = seed
         generator = GKMersenneTwisterRandomSource(seed: seed)
     }
-    func next<T>(upperBound: T) -> T where T: FixedWidthInteger, T: UnsignedInteger {
+    public func next<T>(upperBound: T) -> T where T: FixedWidthInteger, T: UnsignedInteger {
         return T(abs(generator.nextInt(upperBound: Int(upperBound))))
     }
-    func next<T>() -> T where T: FixedWidthInteger, T: UnsignedInteger {
+    public func next<T>() -> T where T: FixedWidthInteger, T: UnsignedInteger {
         return T(abs(generator.nextInt()))
+    }
+    public func next() -> UInt64 {
+        return UInt64(abs(generator.nextInt()))
     }
 }
 
